36b3579166dd2858fa7a02d1a45daacc35dd898d
Roshnadevi <vh11871_bio22@velhightech.com>
2025-01-20 21:47:52 +0530

    Add files via upload

---
+++ num add.py
@@ -0,0 +1,7 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = 15
+>>> num2 = 12
+>>> sum = num1 + num2
+>>> print("Sum of", num1, "and", num2 , "is", sum)
+Sum of 15 and 12 is 27
