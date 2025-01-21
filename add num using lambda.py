a995d5089c1fa71e352af2e88dcccbda1fed991e
Roshnadevi <vh11871_bio22@velhightech.com>
2025-01-21 23:53:25 +0530

    Add files via upload
    Use the lambda function and assign the value

---
+++ add num using lambda.py
@@ -0,0 +1,8 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> add_numbers = lambda X, Y: X + Y
+>>> num1 = 2
+>>> num2 = 11
+>>> result = add_numbers(num1, num2)
+>>> print("The sum of", num1, "and", num2, "is", result)
+The sum of 2 and 11 is 13
