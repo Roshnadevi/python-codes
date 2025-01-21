d47f4107b9ab25717d999f6f2c114a1da8e62e52
Roshnadevi <vh11871_bio22@velhightech.com>
2025-01-21 23:42:36 +0530

    Add files via upload
    Initialize two variables num1, and num2.
    Find sum using the operator.add() by passing arguments and assign to sum.

---
+++ add num using operator.add.py
@@ -0,0 +1,8 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = 15
+>>> num2 = 12
+>>> import operator
+>>> sum = operator.add(num1,num2)
+>>> print("sum of {0} and {1} is {2}" .format(num1, num2, sum))
+sum of 15 and 12 is 27
