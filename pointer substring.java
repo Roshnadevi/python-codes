7ebad5b1e64103ee562a01803c0216a3466755a4
Roshnadevi <vh11871_bio22@velhightech.com>
2025-01-28 19:23:56 +0530

    Add files via upload
    It prints all substring using the iteration and looping

---
+++ pointer substring.java
@@ -0,0 +1,13 @@
+class ex2{
+    public static void main(String[] args){
+        String s1 = "ROSHNA";
+        for(int i=0; i<s1.length(); i++)
+        {
+            for(int j=i+1; j<=s1.length(); j++)
+            {
+                System.out.println(s1.substring(i,j));
+            }
+        }
+    }
+
+}
