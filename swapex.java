01556e454e849ae6db2dd186fba1d8d9ae49f6e3
Roshnadevi <vh11871_bio22@velhightech.com>
2025-01-28 18:54:29 +0530

    Add files via upload
    swapping the number using temporary variable

---
+++ swapex.java
@@ -0,0 +1,11 @@
+// Your First Program
+
+class swapex {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        System.out.println("before swapping:"+a+b); 
+        
+        System.out.println("After swapping:"+a+b); 
+    }
+}
