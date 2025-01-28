0cb551a47b74ba58a38ba90dc9a80014f0993493
Roshnadevi <vh11871_bio22@velhightech.com>
2025-01-28 19:04:09 +0530

    Add files via upload
    odd or even is done using the modulo equalling the zero value and if or else is used to find the results

---
+++ even number.java
@@ -0,0 +1,12 @@
+class oddoreven{
+    public static void main(String[] args){
+        int a=23;
+        if (a%2==0) {
+            System.out.println("even number");
+        }
+        else {
+            System.out.println("odd number"); 
+        }           
+        }    
+        
+    }
