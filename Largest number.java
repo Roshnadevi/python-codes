7fc8691c9b3f6b42c0e52de50d1a2f7aee9c87d3
Roshnadevi <vh11871_bio22@velhightech.com>
2025-01-31 00:35:33 +0530

    Add files via upload
    The program use the Math .max ( ) method and founded the largest number

---
+++ Largest number.java
@@ -0,0 +1,7 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 5, b = 10, c = 3;
+        int largest = Math.max(a, Math.max(b, c));
+        System.out.println(largest + " is the largest number.");
+    }
+}
